9bf512297d44648c416fdcf8ddddd4d05d93ed76
Bendik Øverbø Helseth <95276761+Bendikhelseth@users.noreply.github.com>
2022-12-20 09:31:29 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const button1 = document.querySelector('#btn1');
+
+button1.addEventListener('click', function(){
+    const inputs = document.querySelector('#input1').value
+    document.body.style.backgroundColor = inputs
+    document.querySelector('.textBox2').innerHTML = document.body.style.backgroundColor
+});
+
+button1.addEventListener('dblclick', function(){
+    document.body.style.backgroundColor = "white"
+});
+
+const button2 = document.querySelector('#btn2');
+
+button2.addEventListener('click', function(){
+
+    const colorList = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey"]
+
+    var randomColor = Math.round(Math.random()*colorList.length)
+
+
+    document.body.style.backgroundColor = colorList[randomColor] 
+
+    document.querySelector('.textBox2').innerHTML = document.body.style.backgroundColor
+});
+
+
